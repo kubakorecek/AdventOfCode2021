9b78bf126176d3e815454fc095b58ae73c7c9a0c
Jakub Koreček <jakub.korecek1@gmail.com>
2022-03-22 08:49:53 +0100

    Day1 - Refactored

--- AdventOfCode.Run/RunAOC2021.cs
+++ AdventOfCode.Run/RunAOC2021.cs
@@ -5,17 +5,6 @@
         public static void Main(string[] Args)
         {
             string filepath = @"C:\Users\kubak\source\repos\AdventOfCode\AdventOfCode.Day1\input.txt";
-            if (File.Exists(filepath))
-            {
-                Console.WriteLine("File exists...");
-                Day1.Excercise st = new Day1.Excercise(filepath);
-                st.calc();
-            }
-            else
-            {
-                Console.WriteLine("File does not exist in the current directory!");
-            }
-
 
             filepath = @"C:\Users\kubak\source\repos\AdventOfCode\AdventOfCode.Day2\input.txt";
 
