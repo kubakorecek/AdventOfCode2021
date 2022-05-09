4ce7e02fe1ae9d5f2639a3db631138c55c6281f6
Jakub Koreček <jakub.korecek1@gmail.com>
2022-05-09 23:55:44 +0200

    DAy1 refactored., adding utils sliding winwod

---
+++ AdventOfCode2021_CSharp/Day2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021_CSharp
+{
+    public class Day2 : ILoader
+    {
+        public string FileContent { get; }
+
+        public List<Tuple<string, int>> Instructions { get; set; }
+        public Day2(string fileContent)
+        {
+            FileContent = fileContent;
+        }
+
+        public bool Load()
+        {
+            // TODO: Split in some smart way!!!! in to tuples
+            Instructions = new List<Tuple<string, int>>();
+            FileContent.Split('\n').Split(' ')
+            return true;
+        }
+    }
+}
---
+++ AdventOfCode2021_CSharp/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021_CSharp
+{
+    public static class Utils
+    {
+        public static IEnumerable<int> WindowSlidder(this List<int> numbers, int window, int size)
+        {
+            return numbers.Skip(window).Take(size);
+        }
+    }
+}
