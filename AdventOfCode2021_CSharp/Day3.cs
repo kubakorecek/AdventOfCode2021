fa479e4a0d4bc7cb4c480cba3bbb500a54c1cd11
Jakub Koreček <jakub.korecek1@gmail.com>
2022-05-13 01:00:52 +0200

    day 3 started

--- AdventOfCode2021_CSharp/Day3.cs
+++ AdventOfCode2021_CSharp/Day3.cs
@@ -15,10 +15,44 @@ namespace AdventOfCode2021_CSharp
             FileContent = fileContent;
         }
 
+        public Day3(List<string> binaries)
+        {
+            Binaries = binaries;
+        }
+
         public bool Load()
         {
             Binaries = FileContent.Split('\n').ToList();
             return true;
+        }
+
+        public List<string> theLeastCommon(List<string> data, int pos = -1)
+        {
+            if (pos < 0) { throw new ArgumentException("Parameter cannot be <0", nameof(pos)); }
+
+            List<string> Data0 = new List<string>();
+            List<string> Data1 = new List<string>();
+
+            foreach (string str in data)
+            {
+                ;
+                if (str[pos].Equals('0'))
+                {
+                    Data0.Add(str);
+                }
+                else
+                {
+                    Data1.Add(str);
+                }
+               ;
+
+            }
+            if (Data0.Count > Data1.Count)
+            {
+                return Data0;
+            }
+
+            return Data1;
         }
     }
 }
--- TestAdventOfCode2021_Csharp/UnitTest03.cs
+++ TestAdventOfCode2021_Csharp/UnitTest03.cs
@@ -11,7 +11,7 @@ namespace TestAdventOfCode2021_Csharp
 
         [DataRow(@"data/day3_test.txt", 9, 22, 198)]
         [DataTestMethod]
-        public void TestDay1(string fileName,int expectedRate, int gammaRate, int consumption)
+        public void TestDay1(string fileName, int expectedRate, int gammaRate, int consumption)
         {
             var fileContent = File.ReadAllText(fileName);
             var day1 = new Day1(fileContent);
@@ -20,7 +20,23 @@ namespace TestAdventOfCode2021_Csharp
         }
 
 
+        [TestClass()]
+        public class ExcerciseTests
+        {
+            private List<string> data = new List<string>()
+        {"01110", "11111", "00000"};
+            int position = 2;
+            private List<string> expected = new List<string>()
+        {"01110", "11111"};
 
+            [TestMethod()]
+            public void theLeastCommonTest()
+            {
+                Day3 test = new Day3(data);
+                List<string> res = test.theLeastCommon(data, position);
+                CollectionAssert.AreEqual(expected, res);
+            }
+        }
 
 
 
